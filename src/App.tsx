9ab434799e1a26dc6c3c468f8b9b428753b04661
giselegal <consultoria@giselegalvao.com.br>
2025-07-28 03:26:06 +0000

    atualização 22

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { ConfigProvider } from 'antd';
 import ptBR from 'antd/locale/pt_BR';
 import { TooltipProvider } from '@/components/ui/Tooltip';
 import { AuthProvider } from './context/AuthContext';
+import { Router, Route, Switch } from 'wouter';
 import { MainApp } from './components/MainApp';
+import SchemaDrivenEditorPage from './pages/SchemaDrivenEditorPage';
 import { quizQuestTheme } from './config/antd-theme';
 import './index.css';
 
@@ -13,7 +15,16 @@ function App() {
     <ConfigProvider theme={quizQuestTheme} locale={ptBR}>
       <TooltipProvider>
         <AuthProvider>
-          <MainApp />
+          <Router>
+            <Switch>
+              {/* Rota para o Editor Principal */}
+              <Route path="/editor" component={SchemaDrivenEditorPage} />
+              <Route path="/editor/:id" component={SchemaDrivenEditorPage} />
+              
+              {/* Rota padrão - MainApp (Dashboard) */}
+              <Route component={MainApp} />
+            </Switch>
+          </Router>
         </AuthProvider>
       </TooltipProvider>
     </ConfigProvider>
