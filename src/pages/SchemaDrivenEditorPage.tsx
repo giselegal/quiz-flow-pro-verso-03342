b80426dfd2973b5ff42891b53c607ac259024c5d
giselegal <consultoria@giselegalvao.com.br>
2025-07-28 16:36:16 +0000

    atualização 22

--- src/pages/SchemaDrivenEditorPage.tsx
+++ src/pages/SchemaDrivenEditorPage.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DroppableCanvas } from '@/components/editor/dnd/DroppableCanvas';
 import { FormElementsPanel } from '@/components/editor/FormElementsPanel';
 
 export default function SchemaDrivenEditorPage() {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="flex h-screen">
-        <FormElementsPanel />
-        <div className="flex-1">
-          <DroppableCanvas />
-        </div>
+    <div className="flex h-screen">
+      <FormElementsPanel />
+      <div className="flex-1">
+        <DroppableCanvas />
+      </div>
+    </div>
+  );
+}
       </div>
     </DndProvider>
   );
