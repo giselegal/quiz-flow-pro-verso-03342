b89e516131aef5354b5576aad30689ce8669c391
giselegal <consultoria@giselegalvao.com.br>
2025-07-31 18:31:49 +0000

    atualização 22

--- src/components/ui/optimized-image.tsx
+++ src/components/ui/optimized-image.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useMemo } from 'react';
 import { cn } from '@/lib/utils';
 import { optimizeCloudinaryUrl, getResponsiveImageSources, getLowQualityPlaceholder } from '@/utils/imageUtils';
