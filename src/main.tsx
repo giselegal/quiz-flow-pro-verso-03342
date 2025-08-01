a2352c97c4d16fa05518fdc0a44cabf12dbfacaf
giselegal <consultoria@giselegalvao.com.br>
2025-08-01 05:29:26 +0000

    atualização 22

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+// Filtro de segurança para scripts externos (deve ser carregado primeiro)
+import "./utils/scriptFilter";
+
+// Otimizações de performance para desenvolvimento
+import "./utils/devPerformanceOptimizer";
+
 console.log('🚀 Aplicação iniciando...');
 
 createRoot(document.getElementById("root")!).render(
