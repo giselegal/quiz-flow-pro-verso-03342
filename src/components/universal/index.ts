60417310977908108749d3044ac5c6227ab1f05f
giselegal <consultoria@giselegalvao.com.br>
2025-08-05 21:56:33 +0000

    quis publicado

--- src/components/universal/index.ts
+++ src/components/universal/index.ts
@@ -1,1 +1,10 @@
+// Importa os componentes
+import { EnhancedUniversalPropertiesPanel } from "./EnhancedUniversalPropertiesPanel";
+import SimplifiedUniversalPropertiesPanel from "./SimplifiedUniversalPropertiesPanel";
+
+// Exporta os componentes principais como nomeados
 export { EnhancedUniversalPropertiesPanel, SimplifiedUniversalPropertiesPanel };
+
+// Define EnhancedUniversalPropertiesPanel como a exportação padrão
+// Isso permite que você o importe como: import MeuPainel from "@/components/universal";
+export default EnhancedUniversalPropertiesPanel;
