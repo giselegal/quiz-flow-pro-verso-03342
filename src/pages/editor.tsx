03c383c461c7d029befc6f03436030fa9d113e52
giselegal <consultoria@giselegalvao.com.br>
2025-08-05 14:32:12 +0000

    quis publicado

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -14,6 +14,7 @@ import { useEditor } from "../hooks/useEditor";
 import { cn } from "../lib/utils";
 import { schemaDrivenFunnelService } from "../services/schemaDrivenFunnelService";
 import { normalizeBlock } from "../utils/blockTypeMapping";
+import UniversalBlockRenderer from "../components/editor/blocks/UniversalBlockRenderer";
 
 // Componente simples para renderizar blocos
 const SimpleBlockRenderer: React.FC<{
