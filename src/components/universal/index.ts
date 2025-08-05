b307ac2cebc08b94d96698297944e2f62e7295cc
giselegal <consultoria@giselegalvao.com.br>
2025-08-05 20:07:31 +0000

    quis publicado

--- src/components/universal/index.ts
+++ src/components/universal/index.ts
@@ -1,4 +1,5 @@
 import UniversalPropertiesPanelInternal from "./UniversalPropertiesPanel";
 
+// Re-exporta tanto como exportação nomeada quanto como padrão para compatibilidade
 export const UniversalPropertiesPanel = UniversalPropertiesPanelInternal;
 export default UniversalPropertiesPanelInternal;
