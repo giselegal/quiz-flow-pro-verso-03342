d9721456d2155200da467cfa31a667864cdcd855
giselegal <consultoria@giselegalvao.com.br>
2025-08-05 21:54:14 +0000

    quis publicado

--- src/components/universal/index.ts
+++ src/components/universal/index.ts
@@ -1,17 +1,2 @@
-// Importa os componentes
-import { EnhancedUniversalPropertiesPanel } from "./EnhancedUniversalPropertiesPanel";
-import SimplifiedUniversalPropertiesPanel from "./SimplifiedUniversalPropertiesPanel";
-import { UniversalPropertiesPanel } from "./UniversalPropertiesPanel";
-
-// Exportações internas necessárias para resolver os problemas de importação
-export * from "./UniversalPropertiesPanel";
-
-// Exporta os componentes principais
-export {
-  EnhancedUniversalPropertiesPanel,
-  SimplifiedUniversalPropertiesPanel,
-  UniversalPropertiesPanel,
-};
-
-// Exportação padrão para compatibilidade
-export default SimplifiedUniversalPropertiesPanel;
+import { EnhancedUniversalPropertiesPanel } from "@/components/universal";
+import
