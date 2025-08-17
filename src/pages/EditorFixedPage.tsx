47bd11a959203f9aa58a1629d9736aa0c8ffa75d
giselegal <consultoria@giselegalvao.com.br>
2025-08-17 06:01:34 +0000

    Atualização: melhorias e correções

--- src/pages/EditorFixedPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { EditorWithPreview } from './editor';
-
-const EditorFixedPage: React.FC = () => {
-  return <EditorWithPreview />;
-};
-
-export default EditorFixedPage;
