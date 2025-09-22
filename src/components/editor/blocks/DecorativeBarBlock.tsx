4cce3eedff982b10b5410da94ed63cc2e261e7cf
giselegal <consultoria@giselegalvao.com.br>
2025-09-22 19:14:34 +0000

    feat: Refatoração de blocos simples para melhorar a legibilidade e a estrutura do código

--- src/components/editor/blocks/DecorativeBarBlock.tsx
+++ src/components/editor/blocks/DecorativeBarBlock.tsx
@@ -5,46 +5,46 @@ import React from 'react';
  * 🎨 DecorativeBarBlock - Barra decorativa
  */
 const DecorativeBarBlock: React.FC<BlockComponentProps> = ({
-  block,
-  isSelected = false,
-  onClick,
-  className = '',
+    block,
+    isSelected = false,
+    onClick,
+    className = '',
 }) => {
-  const {
-    width = '100%',
-    height = '4px',
-    backgroundColor = '#B89B7A',
-    borderRadius = '2px',
-    marginTop = 16,
-    marginBottom = 16,
-  } = block?.properties || {};
+    const {
+        width = '100%',
+        height = '4px',
+        backgroundColor = '#B89B7A',
+        borderRadius = '2px',
+        marginTop = 16,
+        marginBottom = 16,
+    } = block?.properties || {};
 
-  const barStyle = {
-    width: width === 'auto' ? '100%' : width,
-    height: `${height}px`,
-    backgroundColor,
-    borderRadius: `${borderRadius}px`,
-    marginTop: `${marginTop}px`,
-    marginBottom: `${marginBottom}px`,
-    margin: '0 auto',
-  } as React.CSSProperties;
+    const barStyle = {
+        width: width === 'auto' ? '100%' : width,
+        height: `${height}px`,
+        backgroundColor,
+        borderRadius: `${borderRadius}px`,
+        marginTop: `${marginTop}px`,
+        marginBottom: `${marginBottom}px`,
+        margin: '0 auto',
+    } as React.CSSProperties;
 
-  return (
-    <div
-      className={`
+    return (
+        <div
+            className={`
         py-2 px-2 cursor-pointer transition-all duration-200
         ${isSelected ? 'ring-1 ring-blue-400 bg-blue-50/30' : 'hover:bg-gray-50/50'}
         ${className}
       `}
-      onClick={onClick}
-      data-block-id={block?.id}
-      data-block-type={block?.type}
-    >
-      <div style={{ textAlign: 'center' }}>
-        <div style={barStyle} />
-      </div>
-    </div>
-  );
+            onClick={onClick}
+            data-block-id={block?.id}
+            data-block-type={block?.type}
+        >
+            <div style={{ textAlign: 'center' }}>
+                <div style={barStyle} />
+            </div>
+        </div>
+    );
 };
 
 export default DecorativeBarBlock;
--- src/components/editor/blocks/LegalNoticeBlock.tsx
+++ src/components/editor/blocks/LegalNoticeBlock.tsx
@@ -5,50 +5,50 @@ import React from 'react';
  * ⚖️ LegalNoticeBlock - Aviso legal/copyright
  */
 const LegalNoticeBlock: React.FC<BlockComponentProps> = ({
-  block,
-  isSelected = false,
-  onClick,
-  className = '',
+    block,
+    isSelected = false,
+    onClick,
+    className = '',
 }) => {
-  const {
-    fontSize = '12px',
-    color = '#666666',
-    textAlign = 'center',
-    marginTop = 16,
-    marginBottom = 16,
-    paddingTop = 8,
-    paddingBottom = 8,
-  } = block?.properties || {};
+    const {
+        fontSize = '12px',
+        color = '#666666',
+        textAlign = 'center',
+        marginTop = 16,
+        marginBottom = 16,
+        paddingTop = 8,
+        paddingBottom = 8,
+    } = block?.properties || {};
 
-  // Usar texto do content ou padrão
-  const legalText = block?.content?.text || '2025 - Todos os direitos reservados';
+    // Usar texto do content ou padrão
+    const legalText = block?.content?.text || '2025 - Todos os direitos reservados';
 
-  const containerStyle = {
-    fontSize,
-    color,
-    textAlign,
-    marginTop: `${marginTop}px`,
-    marginBottom: `${marginBottom}px`,
-    paddingTop: `${paddingTop}px`,
-    paddingBottom: `${paddingBottom}px`,
-  } as React.CSSProperties;
+    const containerStyle = {
+        fontSize,
+        color,
+        textAlign,
+        marginTop: `${marginTop}px`,
+        marginBottom: `${marginBottom}px`,
+        paddingTop: `${paddingTop}px`,
+        paddingBottom: `${paddingBottom}px`,
+    } as React.CSSProperties;
 
-  return (
-    <div
-      className={`
+    return (
+        <div
+            className={`
         py-2 px-2 cursor-pointer transition-all duration-200
         ${isSelected ? 'ring-1 ring-blue-400 bg-blue-50/30' : 'hover:bg-gray-50/50'}
         ${className}
       `}
-      onClick={onClick}
-      data-block-id={block?.id}
-      data-block-type={block?.type}
-    >
-      <div style={containerStyle}>
-        <p dangerouslySetInnerHTML={{ __html: legalText }} />
-      </div>
-    </div>
-  );
+            onClick={onClick}
+            data-block-id={block?.id}
+            data-block-type={block?.type}
+        >
+            <div style={containerStyle}>
+                <p dangerouslySetInnerHTML={{ __html: legalText }} />
+            </div>
+        </div>
+    );
 };
 
 export default LegalNoticeBlock;
