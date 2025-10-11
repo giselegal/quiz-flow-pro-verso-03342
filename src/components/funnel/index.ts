a96515cd241e06945c22ab694ac3bd07e196e7ea
giselegal <consultoria@giselegalvao.com.br>
2025-10-11 00:30:26 +0000

    feat: adicionar exportações de componentes de funil

---
+++ src/components/funnel/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 🎯 FUNNEL - Barrel Exports
+ * 
+ * Componentes de funis de vendas.
+ */
+
+// Base Components
+export { default as FunnelHeroSection } from './base/FunnelHeroSection';
+export { default as FunnelPainSection } from './base/FunnelPainSection';
