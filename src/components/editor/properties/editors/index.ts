3ad36bee0a54b5988a042de14bcfec621f9cc024
giselegal <consultoria@giselegalvao.com.br>
2025-10-17 13:52:57 +0000

    feat: adicionar editores de propriedades para blocos de transição e resultado

--- src/components/editor/properties/editors/index.ts
+++ src/components/editor/properties/editors/index.ts
@@ -6,3 +6,13 @@ export { OptionsPropertyEditor } from './OptionsPropertyEditor';
 export { QuestionPropertyEditor } from './QuestionPropertyEditor';
 export { TextPropertyEditor } from './TextPropertyEditor';
 export { default as ImagePropertyEditor } from './ImagePropertyEditor';
+
+// Editores de blocos de transição
+export { LoaderPropertyEditor } from './LoaderPropertyEditor';
+export { ProgressPropertyEditor } from './ProgressPropertyEditor';
+export { MessagePropertyEditor } from './MessagePropertyEditor';
+
+// Editores de blocos de resultado
+export { StyleResultPropertyEditor } from './StyleResultPropertyEditor';
+export { CharacteristicsPropertyEditor } from './CharacteristicsPropertyEditor';
+export { SecondaryStylesPropertyEditor } from './SecondaryStylesPropertyEditor';
