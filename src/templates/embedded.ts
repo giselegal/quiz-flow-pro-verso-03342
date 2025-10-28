5ada5231eb1554fa70e6c5974418ccfa69983efc
giselegal <consultoria@giselegalvao.com.br>
2025-10-28 23:41:57 +0000

    feat: atualizar timestamps de geração nos templates embedded e quiz21StepsComplete

--- src/templates/embedded.ts
+++ src/templates/embedded.ts
@@ -1,7 +1,7 @@
 /**
  * 🏗️ BUILD-TIME TEMPLATES EMBEDDED
  * 
- * Gerado automaticamente em: 2025-10-28T22:34:55.162Z
+ * Gerado automaticamente em: 2025-10-28T23:41:21.121Z
  * Total de steps: 21
  * Total de blocos: 101
  * 
