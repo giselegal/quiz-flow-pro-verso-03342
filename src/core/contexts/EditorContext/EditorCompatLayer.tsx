7893aa8590db66ebfd05b6ebd435af2af8de9159
giselegal <consultoria@giselegalvao.com.br>
2025-12-02 23:26:34 +0000

    refactor: atualizar importação do tipo Block para melhorar a estrutura do código

--- src/core/contexts/EditorContext/EditorCompatLayer.tsx
+++ src/core/contexts/EditorContext/EditorCompatLayer.tsx
@@ -17,7 +17,7 @@
 
 import React, { useMemo } from 'react';
 import { useEditor as useEditorCanonical, type EditorContextValue } from './EditorStateProvider';
-import type { Block } from '@/types/block.types';
+import type { Block } from '@/types/editor';
 
 export interface EditorCompatAPI extends EditorContextValue {
     // API legada - State Extensions
